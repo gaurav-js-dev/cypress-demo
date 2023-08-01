8e6853f7fd5dc2398369821a506321fb399d9d2e
Gaurav <23628920+gaurav-js-dev@users.noreply.github.com>
2023-08-01 17:38:07 +0530

    Added UI interaction and routing test examples

--- cypress-example-kitchensink/cypress/e2e/1-getting-started/myFirstTest.cy.js
+++ cypress-example-kitchensink/cypress/e2e/1-getting-started/myFirstTest.cy.js
@@ -22,4 +22,48 @@ context("Actions", () => {
         cy.get("h4").should("exist");
       });
   });
+
+  it("show an active class for current pag menu", () => {
+    cy.get(".dropdown-menu").find("li").eq(2).should("have.class", "active");
+  });
+
+  // Routing and navigation examples
+
+  it("should not show an active class for inactive page menu", () => {
+    cy.get(".dropdown-menu")
+      .find("li")
+      .first()
+      .should("not.have.class", "active")
+      .find("a")
+      .should("have.attr", "href", "/commands/querying");
+  });
+
+  it("navigates to the actions page correctly upon click", () => {
+    cy.visit("/");
+    cy.findAllByText("Actions").last().click();
+    cy.url().should("include", "/commands/actions");
+  });
+
+  // UI Actions Examples Below
+
+  it("lets you type into an input field ", () => {
+    cy.findAllByPlaceholderText("Email")
+      .type("Test")
+      .should("have.value", "Test");
+  });
+
+  it("lets you clear the input field", () => {
+    cy.findAllByLabelText("Describe:")
+      .type("Test Description")
+      .should("have.value", "Test Description")
+      .clear()
+      .should("have.value", "");
+  });
+
+  it("lets you check a checkbox", () => {
+    cy.get('.action-checkboxes [type ="checkbox"]')
+      .eq(2)
+      .check()
+      .should("be.checked");
+  });
 });
